03f803173e7a2a4ef3e12ebb1af2e08cda6cf874
alexdenuke <alexdenuke@gmail.com>
2023-03-25 18:38:23 +0300

    product-card'

--- script/script.js
+++ script/script.js
@@ -1,24 +1,8 @@
 let catalog = document.querySelector('.header__catalog');
 let dropdown = document.querySelector('.dropdown');
 
-
-let burger = document.querySelector('.header__burger');
-let burgerBody = document.querySelector('.burger__body');
-
-burger.addEventListener('click', ()=> {
-    
-    burgerBody.classList.toggle('show');
-    document.body.classList.toggle('lock');
-    // if (burgerBody.classList.contains('show')) {
-    //     document.body.classList.add("lock");
-    // }
-    // else {
-    //     document.body.classList.remove('lock');
-    // }
-    
-});
 window.onclick = function (event) {
-    if (!event.target.matches('.header__catalog')) {
+    if (!event.target.matches('.header__catalog') ){
         let dropContent = document.querySelectorAll(".dropdown");
         let i;
         for (i = 0; i < dropContent.length; i++) {
@@ -31,6 +15,27 @@ window.onclick = function (event) {
 }
 
 
-catalog.addEventListener('click', ()=> {
+ catalog.addEventListener('click', ()=> {
     dropdown.classList.toggle('active');  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let burger = document.querySelector('.header__burger');
+let burgerBody = document.querySelector('.burger__body');
+burger.addEventListener('click', ()=> {
+    burgerBody.classList.toggle('show');
+    document.body.classList.toggle('lock');
+    
+});
